205871e46861374735d79864a5dbbf3528980612
AlvaroDelgado <alvarodh2011@gmail.com>
2021-03-30 20:27:30 -0600

    picture routes done(will need checking)

--- backend/src/modules/pictures/pictures.controller.ts
+++ backend/src/modules/pictures/pictures.controller.ts
@@ -4,7 +4,7 @@ import prisma from '../../database'
 
 
 export const getPictures:RequestHandler = async (req,res) => {  
-    const pictures = await prisma.location.findMany({where: {id: parseInt(req.params.id)}});
+    const pictures = await prisma.picture.findMany({where: {Tour: parseInt(req.params.tour)}});
     if(!pictures) return res.status(204).json();
     return res.json(pictures);
 };
--- backend/src/modules/pictures/pictures.routes.ts
+++ backend/src/modules/pictures/pictures.routes.ts
@@ -3,7 +3,7 @@ import * as picsCtrl from './pictures.controller'
 
 const router = Router();
 
-router.get('/pictures/:id', picsCtrl.getPictures);
+router.get('/pictures/:tour', picsCtrl.getPictures);
 router.post('/pictures', picsCtrl.createPicture);
 
 export default router;
